1e422c0631706788209ed574cb2789c44634c8f5
cosminmarian53 <cosmin.lavric@student.unitbv.ro>
2025-03-24 21:17:54 +0200

    MinimalAccount.sol added, setup finished

---
+++ README.md
@@ -0,0 +1,7 @@
+# About this project- Account Abstraction
+
+1. Create a basic AA on Ethereum
+2. Create a basic AA on zkSync
+3. Deploy, and send a userOp/ transaction through them
+    a) Not going to send an AA to ETH
+    b) Instead, we will send an AA tx to zkSync
---
+++ src/ethereum/MinimalAccount.sol
@@ -0,0 +1,44 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
+import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+
+contract MinimalAccount is IAccount {
+    // Entrypoint->(calls) this contract
+
+    //a sig valid if MinimalAccount is owner
+    /*//////////////////////////////////////////////////////////////
+                           EXTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
+        external
+        returns (uint256 validationData)
+    {
+        //  _validateSignature(userOp, userOpHash);
+    }
+    /*//////////////////////////////////////////////////////////////
+                           INTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+}
