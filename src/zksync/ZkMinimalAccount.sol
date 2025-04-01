4b5c7959d2b88780d51865a0d909b53db393d09e
cosminmarian53 <cosmin.lavric@student.unitbv.ro>
2025-04-01 22:59:28 +0300

    ZkMinimalAccount tweaks

--- src/zksync/ZkMinimalAccount.sol
+++ src/zksync/ZkMinimalAccount.sol
@@ -23,10 +23,21 @@
 pragma solidity 0.8.24;
 
 // zkSync Era Imports
-import {IAccount, ACCOUNT_VALIDATION_SUCCESS_MAGIC} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/IAccount.sol";
-import {Transaction, MemoryTransactionHelper} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
-import {SystemContractsCaller} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/SystemContractsCaller.sol";
-import {NONCE_HOLDER_SYSTEM_CONTRACT, BOOTLOADER_FORMAL_ADDRESS, DEPLOYER_SYSTEM_CONTRACT} from "lib/foundry-era-contracts/src/system-contracts/contracts/Constants.sol";
+import {
+    IAccount,
+    ACCOUNT_VALIDATION_SUCCESS_MAGIC
+} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/IAccount.sol";
+import {
+    Transaction,
+    MemoryTransactionHelper
+} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
+import {SystemContractsCaller} from
+    "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/SystemContractsCaller.sol";
+import {
+    NONCE_HOLDER_SYSTEM_CONTRACT,
+    BOOTLOADER_FORMAL_ADDRESS,
+    DEPLOYER_SYSTEM_CONTRACT
+} from "lib/foundry-era-contracts/src/system-contracts/contracts/Constants.sol";
 import {INonceHolder} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/INonceHolder.sol";
 import {Utils} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/Utils.sol";
 
@@ -64,6 +75,7 @@ contract ZkMinimalAccount is IAccount, Ownable {
     error ZkMinimalAccount__ExecutionFailed();
     error ZkMinimalAccount__NotFromBootLoaderOrOwner();
     error ZkMinimalAccount__FailedToPayForTransaction();
+    error ZkMinimalAccount__InvalidSignature();
 
     /*//////////////////////////////////////////////////////////////
                                MODIFIERS
@@ -74,6 +86,7 @@ contract ZkMinimalAccount is IAccount, Ownable {
         }
         _;
     }
+
     modifier requireFromBootLoaderOrOwner() {
         if (msg.sender != BOOTLOADER_FORMAL_ADDRESS && msg.sender != owner()) {
             revert ZkMinimalAccount__NotFromBootLoaderOrOwner();
@@ -99,59 +112,58 @@ contract ZkMinimalAccount is IAccount, Ownable {
 
     function validateTransaction(
         bytes32,
-        /*_txHash*/ bytes32,
-        /*_suggestedSignedHash*/ Transaction memory _transaction
+        /*_txHash*/
+        bytes32,
+        /*_suggestedSignedHash*/
+        Transaction memory _transaction
     ) external payable requireFromBootLoader returns (bytes4 magic) {
         return _validateTransaction(_transaction);
     }
 
     function executeTransaction(
         bytes32,
-        /*_txHash*/ bytes32,
-        /*_suggestedSignedHash*/ Transaction memory _transaction
+        /*_txHash*/
+        bytes32,
+        /*_suggestedSignedHash*/
+        Transaction memory _transaction
     ) external payable requireFromBootLoaderOrOwner {
         _executeTransaction(_transaction);
     }
 
-    function executeTransactionFromOutside(
-        Transaction memory _transaction
-    ) external payable {
-        _validateTransaction(_transaction);
+    function executeTransactionFromOutside(Transaction memory _transaction) external payable {
+        bytes4 magic = _validateTransaction(_transaction);
+        if (magic != ACCOUNT_VALIDATION_SUCCESS_MAGIC) {
+            revert ZkMinimalAccount__InvalidSignature();
+        }
         _executeTransaction(_transaction);
     }
 
-    function payForTransaction(
-        bytes32 /*_txHash*/,
-        bytes32 /*_possibleSignedHash*/,
-        Transaction memory _transaction
-    ) external payable {
+    function payForTransaction(bytes32, /*_txHash*/ bytes32, /*_possibleSignedHash*/ Transaction memory _transaction)
+        external
+        payable
+    {
         bool success = _transaction.payToTheBootloader();
         if (!success) {
             revert ZkMinimalAccount__FailedToPayForTransaction();
         }
     }
 
-    function prepareForPaymaster(
-        bytes32 _txHash,
-        bytes32 _possibleSignedHash,
-        Transaction memory _transaction
-    ) external payable {}
+    function prepareForPaymaster(bytes32 _txHash, bytes32 _possibleSignedHash, Transaction memory _transaction)
+        external
+        payable
+    {}
     /*//////////////////////////////////////////////////////////////
                            INTERNAL FUNCTIONS
     //////////////////////////////////////////////////////////////*/
-    function _validateTransaction(
-        Transaction memory _transaction
-    ) internal returns (bytes4 magic) {
+
+    function _validateTransaction(Transaction memory _transaction) internal returns (bytes4 magic) {
         // Call nonceholder
         // Increment the nonce
         SystemContractsCaller.systemCallWithPropagatedRevert(
             uint32(gasleft()),
             address(NONCE_HOLDER_SYSTEM_CONTRACT),
             0,
-            abi.encodeCall(
-                INonceHolder.incrementMinNonceIfEquals,
-                (_transaction.nonce)
-            )
+            abi.encodeCall(INonceHolder.incrementMinNonceIfEquals, (_transaction.nonce))
         );
         // 1. check for fee to pay
         uint256 totalRequiredBalance = _transaction.totalRequiredBalance();
@@ -170,6 +182,7 @@ contract ZkMinimalAccount is IAccount, Ownable {
         // 3. return the "magic" number
         return magic;
     }
+
     function _executeTransaction(Transaction memory _transaction) internal {
         address to = address(uint160(_transaction.to));
         uint128 value = Utils.safeCastToU128(_transaction.value);
@@ -177,24 +190,11 @@ contract ZkMinimalAccount is IAccount, Ownable {
 
         if (to == address(DEPLOYER_SYSTEM_CONTRACT)) {
             uint32 gas = Utils.safeCastToU32(gasleft());
-            SystemContractsCaller.systemCallWithPropagatedRevert(
-                gas,
-                to,
-                value,
-                data
-            );
+            SystemContractsCaller.systemCallWithPropagatedRevert(gas, to, value, data);
         } else {
             bool success;
             assembly {
-                success := call(
-                    gas(),
-                    to,
-                    value,
-                    add(data, 0x20),
-                    mload(data),
-                    0,
-                    0
-                )
+                success := call(gas(), to, value, add(data, 0x20), mload(data), 0, 0)
             }
             if (!success) {
                 revert ZkMinimalAccount__ExecutionFailed();
--- test/zkSync/ZkMinimalAccountTest.t.sol
+++ test/zkSync/ZkMinimalAccountTest.t.sol
@@ -4,76 +4,57 @@ pragma solidity 0.8.24;
 import {Test} from "forge-std/Test.sol";
 import {ZkMinimalAccount} from "src/zksync/ZkMinimalAccount.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
-import {Transaction, MemoryTransactionHelper} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
+import {
+    Transaction,
+    MemoryTransactionHelper
+} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
 import {BOOTLOADER_FORMAL_ADDRESS} from "lib/foundry-era-contracts/src/system-contracts/contracts/Constants.sol";
-import {ACCOUNT_VALIDATION_SUCCESS_MAGIC} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/IAccount.sol";
+import {ACCOUNT_VALIDATION_SUCCESS_MAGIC} from
+    "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/IAccount.sol";
+
 contract ZkMinimalAccountTest is Test {
     ZkMinimalAccount minimalAccount;
     ERC20Mock usdc;
     uint256 public constant AMOUNT = 1e18;
     bytes32 constant EMPTY_BYTES32 = bytes32(0);
-    address constant ANVIL_DEFAULT_ACCOUNT =
-        0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+    address constant ANVIL_DEFAULT_ACCOUNT = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+
     function setUp() public {
         minimalAccount = new ZkMinimalAccount();
         minimalAccount.transferOwnership(ANVIL_DEFAULT_ACCOUNT);
         usdc = new ERC20Mock();
         vm.deal(address(minimalAccount), AMOUNT);
     }
+
     function testZkOwnerCanExecuteCommands() public {
         // Arrange
         address dest = address(usdc);
         uint256 value = 0;
-        bytes memory functionData = abi.encodeWithSelector(
-            ERC20Mock.mint.selector,
-            address(minimalAccount),
-            AMOUNT
-        );
+        bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
 
-        Transaction memory transaction = _createUnsignedTransaction(
-            minimalAccount.owner(),
-            113,
-            dest,
-            value,
-            functionData
-        );
+        Transaction memory transaction =
+            _createUnsignedTransaction(minimalAccount.owner(), 113, dest, value, functionData);
 
         // Act
         vm.prank(minimalAccount.owner());
-        minimalAccount.executeTransaction(
-            EMPTY_BYTES32,
-            EMPTY_BYTES32,
-            transaction
-        );
+        minimalAccount.executeTransaction(EMPTY_BYTES32, EMPTY_BYTES32, transaction);
 
         // Assert
         assertEq(usdc.balanceOf(address(minimalAccount)), AMOUNT);
     }
+
     function testZkValidateTransaction() public {
         // Arrange
         address dest = address(usdc);
         uint256 value = 0;
-        bytes memory functionData = abi.encodeWithSelector(
-            ERC20Mock.mint.selector,
-            address(minimalAccount),
-            AMOUNT
-        );
+        bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
 
-        Transaction memory transaction = _createUnsignedTransaction(
-            minimalAccount.owner(),
-            113,
-            dest,
-            value,
-            functionData
-        );
+        Transaction memory transaction =
+            _createUnsignedTransaction(minimalAccount.owner(), 113, dest, value, functionData);
         transaction = _signTransaction(transaction);
         // Act
         vm.prank(BOOTLOADER_FORMAL_ADDRESS);
-        bytes4 magic = minimalAccount.validateTransaction(
-            EMPTY_BYTES32,
-            EMPTY_BYTES32,
-            transaction
-        );
+        bytes4 magic = minimalAccount.validateTransaction(EMPTY_BYTES32, EMPTY_BYTES32, transaction);
         // Assert
         assertEq(magic, ACCOUNT_VALIDATION_SUCCESS_MAGIC);
     }
@@ -89,32 +70,28 @@ contract ZkMinimalAccountTest is Test {
         bytes memory functionData
     ) internal view returns (Transaction memory) {
         uint256 nonce = vm.getNonce(address(minimalAccount));
-        return
-            Transaction({
-                txType: transactionType,
-                from: uint256(uint160(from)),
-                to: uint256(uint160(to)),
-                gasLimit: 16777216,
-                gasPerPubdataByteLimit: 16777216,
-                maxFeePerGas: 16777216,
-                maxPriorityFeePerGas: 16777216,
-                paymaster: 0,
-                nonce: nonce,
-                value: value,
-                reserved: [uint256(0), uint256(0), uint256(0), uint256(0)],
-                data: functionData,
-                signature: hex"",
-                factoryDeps: new bytes32[](0),
-                paymasterInput: hex"",
-                reservedDynamic: hex""
-            });
+        return Transaction({
+            txType: transactionType,
+            from: uint256(uint160(from)),
+            to: uint256(uint160(to)),
+            gasLimit: 16777216,
+            gasPerPubdataByteLimit: 16777216,
+            maxFeePerGas: 16777216,
+            maxPriorityFeePerGas: 16777216,
+            paymaster: 0,
+            nonce: nonce,
+            value: value,
+            reserved: [uint256(0), uint256(0), uint256(0), uint256(0)],
+            data: functionData,
+            signature: hex"",
+            factoryDeps: new bytes32[](0),
+            paymasterInput: hex"",
+            reservedDynamic: hex""
+        });
     }
-    function _signTransaction(
-        Transaction memory _transaction
-    ) internal view returns (Transaction memory) {
-        bytes32 unsignedTransactionHash = MemoryTransactionHelper.encodeHash(
-            _transaction
-        );
+
+    function _signTransaction(Transaction memory _transaction) internal view returns (Transaction memory) {
+        bytes32 unsignedTransactionHash = MemoryTransactionHelper.encodeHash(_transaction);
         uint8 v;
         bytes32 r;
         bytes32 s;
